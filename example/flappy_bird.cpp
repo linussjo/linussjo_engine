218f10e068267a7a93f02a9c22f452bd5f2a295f
Linus Sjöström <lisj@netlight.se>
2019-12-22 13:05:39 +0100

    fixed new tests and changed responsiblility of creating the window and settings to linussjo_engine from graphic_engine

--- example/flappy_bird.cpp
+++ example/flappy_bird.cpp
@@ -12,7 +12,7 @@ flappy_bird::flappy_bird(unsigned int w, unsigned int h) : world(w, h){}
 
 void flappy_bird::first_prepare(engine::graphic::graphic_engine &ge, engine::physic::physics_engine &pe)
 {
-    auto r_shape = std::make_shared<engine::graphic::shape::texture>(engine::math::vector2d{0,0}, std::string(IMG_RESOURCE+"/8-bit_Andreas.png"), 3);
+    auto r_shape = std::make_shared<engine::graphic::shape::texture>(engine::math::vector2d{0,0}, std::string(IMG_RESOURCE+"/8-bit_JumpAndreasRight.png"), 3);
     auto po = std::make_shared<engine::physic::physical_object>(r_shape);
     
     po->pos = engine::math::vector2d{200,350};
@@ -28,18 +28,18 @@ void flappy_bird::first_prepare(engine::graphic::graphic_engine &ge, engine::phy
         this->continue_run = false;
     };
 
+    this->create_obstacles(ge, pe);
     this->character = po;
     this->append_po_object(ge, pe, po);
-    this->create_obstacles(ge, pe);
 }
 
 void flappy_bird::create_obstacles(engine::graphic::graphic_engine &ge, engine::physic::physics_engine &pe)
 {
-    int gap_size = 150;
+    int gap_size = 175;
     int min_size = 100;
     int r1 = min_size + rand() % (this->height-gap_size-min_size*3);
     int r2 = this->height-gap_size-r1;
-    printf("%d %d %d %d\n", r1, r2, (this->height-gap_size-min_size*3), (r1+r2+gap_size));
+
     auto re3 = std::make_shared<engine::graphic::shape::rectangle>(engine::math::vector2d{0,0}, r1,75);
     auto po3 = std::make_shared<engine::physic::physical_object>(re3);
     po3->height = r1;
@@ -48,9 +48,8 @@ void flappy_bird::create_obstacles(engine::graphic::graphic_engine &ge, engine::
     po3->is_affected_by_gravity = false;
     po3->is_static = true;
     po3->velocity = engine::math::vector2f{-200, 0};
-    po3->on_out_of_bounds = [](const std::weak_ptr<engine::physic::physical_object> &obs){
-        auto po = obs.lock();
-        po->pos = engine::math::vector2d{995,0};
+    po3->on_out_of_bounds = [this, &ge, &pe](const std::weak_ptr<engine::physic::physical_object> &obs){
+        
     };
     
     this->append_po_object(ge, pe, po3);
@@ -64,9 +63,10 @@ void flappy_bird::create_obstacles(engine::graphic::graphic_engine &ge, engine::
     po4->is_affected_by_gravity = false;
     po4->is_static = true;
     po4->velocity = engine::math::vector2f{-200, 0};
-    po4->on_out_of_bounds = [](const std::weak_ptr<engine::physic::physical_object> &obs){
-        auto po = obs.lock();
-        po->pos = engine::math::vector2d{995,450};
+    po4->on_out_of_bounds = [this, po3, &ge, &pe](const std::weak_ptr<engine::physic::physical_object> &obs){
+        this->remove_po_object(ge, pe, obs.lock());
+        this->remove_po_object(ge, pe, po3);
+        this->create_obstacles(ge, pe);
     };
     
     this->append_po_object(ge, pe, po4);
@@ -81,9 +81,13 @@ void flappy_bird::prepare(engine::graphic::graphic_engine &ge, engine::physic::p
 {
     this->character->pos = engine::math::vector2d{500,350};
     this->character->velocity = engine::math::vector2f{0,0};
+    this->remove_po_object(ge, pe, this->upper_obstacle);
+    this->remove_po_object(ge, pe, this->lower_obstacle);
+    this->create_obstacles(ge, pe);
+    
 
-    this->upper_obstacle->pos = engine::math::vector2d{999,0};
-    this->lower_obstacle->pos = engine::math::vector2d{999,450};
+    //this->upper_obstacle->pos = engine::math::vector2d{999,0};
+    //this->lower_obstacle->pos = engine::math::vector2d{999,450};
 }
 
 void flappy_bird::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods,
